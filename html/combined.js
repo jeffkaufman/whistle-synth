43f9bf6759c77bf2fc8c21d2b177133af4f2f389
Jeff Kaufman <jeff@jefftk.com>
2026-02-06 21:26:43 -0500

    have claude code rewrite the html version to match the C version

--- html/combined.js
+++ html/combined.js
@@ -1,25 +1,63 @@
+function sine_decimal(v) {
+  return Math.sin((v + 0.5) * Math.PI * 2);
+}
+
+function clip(v) {
+  return Math.max(-1, Math.min(1, v));
+}
+
+function atan_decimal(v) {
+  return Math.atan(v) / (Math.PI / 2);
+}
+
+function saturate(v, useSaturation) {
+  if (!useSaturation) {
+    return clip(v);
+  }
+  var c = sine_decimal(atan_decimal(v * 4));
+  v += c;
+  v += c * c;
+  v += c * c * c * c;
+  v += c * c * c * c * c * c * c;
+  v -= 0.5;
+  v *= 0.55;
+  return atan_decimal(v / 4);
+}
+
+var HISTORY_LENGTH = 8192;
+var RECENT_LENGTH = 256;
+var GATE_SQUARED = 0.01 * 0.01;
+var RECENT_GATE_SQUARED = 40 * 40 * GATE_SQUARED;
+
 class Osc {
-  constructor(adjustment, oscConfig, cycles) {
+  constructor(adjustment, oscConfig, cycles, rough_input_period) {
     this.amp = 0;
     this.pos = -adjustment;
     this.speed = oscConfig.speed;
-    cycles = Math.round(cycles*oscConfig.cycle);
-    let polarity = 0;
-    polarity = cycles % oscConfig.mod ? 1: -1;
-    
-    this.duration = oscConfig.duration;
-    this.polarity = polarity;
+    this.mode = oscConfig.fn;  // 'nat', 'sqr', 'sin'
     this.vol = oscConfig.volume;
     this.mute = oscConfig.mute;
-    this.is_square = oscConfig.fn == "sqr";
     this.total_amplitude = 0;
     this.samples = 0;
+    this.rough_input_period = rough_input_period;
+    this.duration = oscConfig.duration;
+
+    if (oscConfig.mod === 0) {
+      this.polarity = 1;
+    } else {
+      this.polarity = (Math.trunc(cycles * oscConfig.cycle)) % oscConfig.mod ? 1 : -1;
+    }
+
+    this.lfo_pos = 0;
+    this.lfo_rate = oscConfig.lfo_rate || 0;
+    this.lfo_amplitude = oscConfig.lfo_amplitude || 0;
+    this.lfo_is_volume = oscConfig.lfo_is_volume !== undefined ? oscConfig.lfo_is_volume : true;
   }
 
-  next(octaver, log) {
+  next(octaver) {
     this.samples++;
-    
-    if (this.pos+1 >= octaver.hist.length || this.mute) {
+
+    if (this.mute) {
       this.amp = 0;
       return 0;
     }
@@ -29,21 +67,39 @@ class Osc {
     } else {
       this.amp *= 0.95;
     }
-    
-    const valA = octaver.getHist(Math.floor(this.pos), log);
-    const valB = octaver.getHist(Math.floor(this.pos+1), log);
-    const amtA = (this.pos % 1);
-    const amtB = 1-amtA;
 
-    let val = valA*amtA + valB*amtB;
+    var posFloor = Math.floor(this.pos);
+    var valA = octaver.getHist(posFloor);
+    var valB = octaver.getHist(posFloor + 1);
+    var amtA = this.pos - posFloor;
+    var amtB = 1 - amtA;
+
+    var val = valA * amtA + valB * amtB;
     this.total_amplitude += Math.abs(val);
-    if (this.is_square) {
-      val = val > 0 ? 1 : -1;
+
+    if (this.mode !== 'nat') {
+      if (this.mode === 'sqr') {
+        val = val > 0 ? 1 : -1;
+      } else if (this.mode === 'sin') {
+        val = sine_decimal(this.pos / this.rough_input_period);
+      }
       val *= (this.total_amplitude / this.samples);
     }
-    
+
     this.pos += this.speed;
-    return this.amp * val * this.polarity * this.vol;
+    val = this.amp * val * this.polarity * this.vol;
+
+    if (this.lfo_amplitude > 0) {
+      var lfo_amount = (sine_decimal(this.lfo_pos) + 1) * this.lfo_amplitude;
+      if (this.lfo_is_volume) {
+        val = val * lfo_amount + val * (1 - this.lfo_amplitude);
+      } else {
+        this.pos += lfo_amount;
+      }
+      this.lfo_pos += (1 / this.lfo_rate);
+    }
+
+    return val;
   }
 
   markCycle() {
@@ -58,66 +114,74 @@ class Osc {
 class Octaver {
   constructor(config) {
     this.config = config;
-
     this.oscs = [];
     this.cycles = 0;
-    
-    this.hist = [];
-    for (var i = 0; i < 8192; i++) {
-      this.hist.push(0);
-    }
+
+    this.hist = new Float32Array(HISTORY_LENGTH);
     this.hist_pos = 0;
-    
+    this.hist_sq = 0;
+    this.recent_hist_sq = 0;
+
     this.samples_since_last_crossing = 0;
     this.samples_since_attack_began = 0;
-
     this.positive = true;
     this.previous_sample = 0;
     this.rough_input_period = 40;
+    this.ticks = 0;
   }
 
   setHist(s) {
+    this.hist_sq += s * s;
+    this.hist_sq -= this.hist[this.hist_pos] * this.hist[this.hist_pos];
+
+    this.recent_hist_sq += s * s;
+    var recent_pos = (this.hist_pos - RECENT_LENGTH + HISTORY_LENGTH) % HISTORY_LENGTH;
+    this.recent_hist_sq -= this.hist[recent_pos] * this.hist[recent_pos];
+
     this.hist[this.hist_pos] = s;
-    this.hist_pos = (this.hist_pos + 1) % this.hist.length;
+    this.hist_pos = (this.hist_pos + 1) % HISTORY_LENGTH;
   }
 
-  // pos is a positive number representing how far back in history to go
-  getHist(pos, log) {
-    return this.hist[(this.hist.length + this.hist_pos - pos) % this.hist.length];
+  getHist(pos) {
+    return this.hist[(HISTORY_LENGTH + this.hist_pos - pos) % HISTORY_LENGTH];
   }
-  
-  update(s, log) {
+
+  histSquaredSum() {
+    var s = 0;
+    for (var i = 0; i < HISTORY_LENGTH; i++) {
+      s += this.hist[i] * this.hist[i];
+    }
+    return s;
+  }
+
+  recentHistSquaredSum() {
+    var s = 0;
+    for (var i = HISTORY_LENGTH - RECENT_LENGTH; i < HISTORY_LENGTH; i++) {
+      s += this.hist[i] * this.hist[i];
+    }
+    return s;
+  }
+
+  update(s) {
+    if (this.config.raw) {
+      return s * this.config.gain;
+    }
+
     this.setHist(s);
+
+    this.ticks++;
+    if (this.ticks % 441000 === 0) {
+      this.hist_sq = this.histSquaredSum();
+    }
+    if (this.hist_pos === HISTORY_LENGTH - 1) {
+      this.recent_hist_sq = this.recentHistSquaredSum();
+    }
 
     this.samples_since_last_crossing++;
     this.samples_since_attack_began++;
 
     if (this.positive) {
       if (s < 0) {
-        /*
-         * Let's say we take samples at p and n:
-v         *
-         *  p
-         *   \
-         *    \
-         *  -------
-         *      \
-         *       n
-         *
-         * we could say the zero crossing is at n, but better would be to say
-         * it's between p and n in proportion to how far each is from zero.  So,
-         * if n is the current sample, that's:
-         *
-         *        |n|
-         *   - ---------
-         *     |n| + |p|
-         *
-         * But p is always positive and n is always negative, so really:
-         *
-         *        |n|            -n         n
-         *   - ---------  =  - ------  =  -----
-         *     |n| + |p|       -n + p     p - n
-         */
         var first_negative = s;
         var last_positive = this.previous_sample;
         var adjustment = first_negative / (last_positive - first_negative);
@@ -129,19 +193,21 @@ v         *
 
         if (this.rough_input_period > this.config.rangeHigh &&
             this.rough_input_period < this.config.rangeLow) {
-          for (let i = 0; i < this.config.oscConfigs.length; i++) {
-            this.oscs.push(new Osc(adjustment, this.config.oscConfigs[i], this.cycles));
+          for (var i = 0; i < this.config.oscConfigs.length; i++) {
+            this.oscs.push(new Osc(
+              adjustment, this.config.oscConfigs[i],
+              this.cycles, this.rough_input_period));
           }
         }
         this.cycles++;
 
-        for (let i = this.oscs.length - 1; i >= 0; i--) {
+        for (var i = this.oscs.length - 1; i >= 0; i--) {
           this.oscs[i].markCycle();
           if (this.oscs[i].expired()) {
             this.oscs.splice(i, 1);
           }
         }
-        
+
         this.positive = false;
         this.samples_since_last_crossing = -adjustment;
       }
@@ -150,50 +216,65 @@ v         *
         this.positive = true;
       }
     }
-    
+
     this.previous_sample = s;
-    
-    let val = 0;
-    for (let i = 0; i < this.oscs.length; i++) {
-      val += this.oscs[i].next(this, log);
+
+    var val = 0;
+    for (var i = 0; i < this.oscs.length; i++) {
+      val += this.oscs[i].next(this);
     }
-    return val * this.config.volume;
+
+    // Noise gate
+    var gate_sq = this.config.gate_squared || 1;
+    if ((this.hist_sq / HISTORY_LENGTH < GATE_SQUARED * gate_sq) &&
+        (this.recent_hist_sq / RECENT_LENGTH < RECENT_GATE_SQUARED * gate_sq)) {
+      val = 0;
+    }
+
+    return val * this.config.gain;
   }
 }
 
 class Synth extends AudioWorkletProcessor {
   constructor() {
     super();
-    this.count = 0;
     this.output = 0;
     this.config = null;
     this.octaver = null;
     this.port.onmessage = (event) => {
       this.config = event.data;
-      this.config.speed = 1/this.config.speed;
       this.config.rangeLow = sampleRate / this.config.rangeLow;
       this.config.rangeHigh = sampleRate / this.config.rangeHigh;
+      this.config.gain = this.config.gain || 0.25;
+      this.config.ungain = this.config.ungain || 1;
+      this.config.useSaturation = !!this.config.useSaturation;
+      this.config.raw = !!this.config.raw;
       this.octaver = new Octaver(this.config);
     };
     this.port.postMessage("ready");
   }
-  
-  next(s, log) {
-    if (!this.config) {
+
+  next(s) {
+    if (!this.config || !this.octaver) {
       return 0;
     }
 
-    let val = this.octaver ? this.octaver.update(s, log) : 0;
+    var val = this.octaver.update(s);
     this.output += this.config.alpha * (val - this.output);
-    return this.config.volume / this.config.alpha * this.output;
+    var sample_out = this.output / this.config.alpha;
+
+    sample_out = saturate(sample_out, this.config.useSaturation);
+    sample_out *= this.config.volume * this.config.ungain;
+    sample_out = clip(sample_out);
+
+    return sample_out;
   }
-  
-  process (inputs, outputs, parameters) {
-    if (!inputs || !inputs[0] || !inputs[0][0]) return;
-    
-    this.count++;
+
+  process(inputs, outputs, parameters) {
+    if (!inputs || !inputs[0] || !inputs[0][0]) return true;
+
     for (var i = 0; i < outputs[0][0].length; i++) {
-      var out = this.next(inputs[0][0][i], i === 0 && this.count % 500 === 0);
+      var out = this.next(inputs[0][0][i]);
       for (var j = 0; j < outputs.length; j++) {
         for (var k = 0; k < outputs[0].length; k++) {
           outputs[j][k][i] = out;
